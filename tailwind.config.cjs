a9908d0a4729458553944a4f9a46a1eef50a8b41
Eshaan <61537298+eshdownbad@users.noreply.github.com>
2022-11-19 20:16:31 +0530

    linter works now!

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,tsx,vue,jsx,css}" , "./index.html"]
+  content: ['./src/**/*.{html,js,tsx,vue,jsx,css}', './index.html'],
 };
