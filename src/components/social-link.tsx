ac2611411e119c5ea9ac811163d17ec11609bed9
Eshaan <61537298+eshdownbad@users.noreply.github.com>
2022-11-27 19:09:54 +0530

    fixed hover animation twitching

--- src/components/social-link.tsx
+++ src/components/social-link.tsx
@@ -9,10 +9,10 @@ export const SocialLink: FunctionComponent<ISocialLinkProps> = props => {
   const { icon, title } = props;
 
   return (
-    <article class='group'>
+    <article class='group/root'>
       <div
         class={
-          'w-full mx-auto rounded-xl mt-4 ' +
+          'w-full mx-auto rounded-xl mt-4 group ' +
           (props.backgroundAccent || 'border')
         }>
         <a
@@ -22,13 +22,13 @@ export const SocialLink: FunctionComponent<ISocialLinkProps> = props => {
               props.onClick(ev);
             }
           }}
-          class='flex bg-black border rounded-xl flex-row w-full p-3 items-center justify-center text-2xl hover:bg-white hover:text-black duration-300 hover:translate-x-2 hover:translate-y-[-0.5rem]'>
+          class='flex bg-black border rounded-xl flex-row w-full p-3 items-center justify-center text-2xl group-hover:bg-white group-hover:text-black duration-300 group-hover:translate-x-2 group-hover:translate-y-[-0.5rem]'>
           <span class='pr-1 pt-1'>{icon}</span>
           <span class='flex-1 text-center'>{title}</span>
         </a>
       </div>
       {props.subtitle ? (
-        <div class='text-center hidden group-hover:block text-gray-400 p-0'>
+        <div class='text-center hidden group-hover/root:block text-gray-400 p-0'>
           <span> {props.subtitle}</span>
         </div>
       ) : null}
