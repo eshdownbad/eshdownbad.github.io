a05241542513b63d3b9e672107759bee60d53ebd
Eshaan <61537298+eshdownbad@users.noreply.github.com>
2022-12-21 12:13:57 +0530

    fixed title cropping issue on mobile

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -2,7 +2,7 @@ import { FunctionalComponent } from 'preact';
 
 export const Title: FunctionalComponent = () => {
   return (
-    <h1 class='animate__animated animate__slideInDown relative mb-3 text-6xl font-bold text-white lg:text-8xl'>
+    <h1 class='animate__animated animate__slideInDown relative mb-3 text-5xl font-bold text-white md:text-6xl lg:text-8xl'>
       <span>EshDownBad</span>
     </h1>
   );
